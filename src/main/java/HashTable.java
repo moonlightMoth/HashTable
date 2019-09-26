e7f1bfd5809ee78a6cb9ccf6716362b7a42a4898
moonlightMoth <ghjpa@inbox.ru>
2019-09-26 18:01:04 +0300

    init

---
+++ src/main/java/HashTable.java
@@ -0,0 +1,77 @@
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
+
+public class HashTable <K, V> implements Map<K, V>
+{
+    private float loadFactor;
+
+
+    
+
+    public Set<Entry<K, V>> entrySet()
+    {
+        return null;
+    }
+
+    public int size()
+    {
+        return 0;
+    }
+
+    public boolean isEmpty()
+    {
+        return false;
+    }
+
+    public boolean containsKey(Object o)
+    {
+        return false;
+    }
+
+    public boolean containsValue(Object o)
+    {
+        return false;
+    }
+
+    public V get(Object o)
+    {
+        return null;
+    }
+
+    public V put(K k, V v)
+    {
+        return null;
+    }
+
+    public boolean remove(Object o, Object o1)
+    {
+        return false;
+    }
+
+    public V remove(Object o)
+    {
+        return null;
+    }
+
+    public void putAll(Map<? extends K, ? extends V> map)
+    {
+
+    }
+
+    public void clear()
+    {
+
+    }
+
+    public Set<K> keySet()
+    {
+        return null;
+    }
+
+    public Collection<V> values()
+    {
+        return null;
+    }
+}
